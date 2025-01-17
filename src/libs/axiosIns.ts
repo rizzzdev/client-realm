1260f5cf96c28fc03f54cf5aec5b9aae5562e012
Rizzz Maooo <rizqonmaulana05@gmail.com>
2025-01-17 22:44:33 +0700

    UPDATE

--- src/libs/axiosIns.ts
+++ src/libs/axiosIns.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const axiosIns = axios.create({
-  withCredentials: false,
+  withCredentials: true,
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-    "Access-Control-Allow-Origin": "*",
-  },
 });
 
 export default axiosIns;

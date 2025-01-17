27d45289a3d6e54e25ad6314484605a129d4f1be
Rizzz Maooo <rizqonmaulana05@gmail.com>
2025-01-17 19:57:13 +0700

    update

--- src/libs/axiosIns.ts
+++ src/libs/axiosIns.ts
@@ -1,8 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosIns = axios.create({
   withCredentials: true,
-  baseURL: process.env.NEXT_PUBLIC_API_URL
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    post: {
+      "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+});
 
-export default axiosIns
+export default axiosIns;

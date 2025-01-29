5c6b71d4d44143cc4a6b84abe8a59d061ba51e3b
Rizzz Maooo <rizqonmaulana05@gmail.com>
2025-01-29 20:44:42 +0700

    update fix bug

--- src/components/layouts/LearningCard.tsx
+++ src/components/layouts/LearningCard.tsx
@@ -22,7 +22,7 @@ const LearningCard = ({
   };
 
   return (
-    <div className="w-full h-fit flex flex-col justify-between items-center bg-background text-primary border-[2px] border-secondary rounded-md overflow-hidden hover:shadow-md hover:shadow-secondary hover:-translate-y-1">
+    <div className="w-full h-full flex flex-col justify-between items-center bg-background text-primary border-[2px] border-secondary rounded-md overflow-hidden hover:shadow-md hover:shadow-secondary hover:-translate-y-1">
       <Image
         src={imageUrl}
         alt="img"
@@ -31,8 +31,10 @@ const LearningCard = ({
         className="w-full aspect-video object-cover"
       />
       <div className="w-full px-5 text-color5">
-        <h3 className="w-full font-bold text-center my-4">{title}</h3>
-        <p className="w-full text-center text-sm">{description}</p>
+        <div className="w-full flex flex-col justify-between items-center gap-4 my-4">
+          <h3 className="w-full font-bold text-center">{title}</h3>
+          <p className="w-full text-center text-sm">{description}</p>
+        </div>
         <p className="w-full text-center text-sm mt-8 font-bold">
           Baca selengkapnya di bawah ini!
         </p>
